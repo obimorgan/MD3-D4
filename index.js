c5e54ed2337347d9a186240a467437eddac4059d
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-28 16:58:33 +0200

    added search bar.

--- index.js
+++ index.js
@@ -79,3 +79,55 @@ const skipCards2 = () => {
 
 // const parentCard = e.target.parentElement
 // cartContainer.appendChild(parentCard)
+
+const searchInput = () => {
+    fetch("https://striveschool-api.herokuapp.com/books/")
+    .then((response) => response.json())
+    .then((body) => { 
+        console.log(body)
+        
+        const searchInput = document.querySelector('.form-control')
+        const productContainer = document.querySelector('.product-container')
+
+        const matchSearch = () => {
+            if (searchInput.value === body.img.title) {
+                
+            }
+
+        }
+        productContainer.innerHTML = body.filter((matchSearch) => 
+            `
+            <div class="card col-sm-12 col-md-4">
+                <img src="${image.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  
+                </div>
+                <a href="#" class="btn btn-success add-btn">Add to Cart</a>
+                <a href="#" class="btn btn-danger skip-btn">Skip</a>
+            </div>
+            `
+        ).join('')
+    })
+    .then(skipCards3)
+    .then(addCardsToCart1)
+    .catch(error => displayErrorMessage(error))
+}
+const addCardsToCart1 = () => {
+    const addBtn = document.querySelectorAll('.add-btn')
+    const cartContainer = document.querySelector('.cart-container')
+    addBtn.forEach(btn => btn.addEventListener('click', e => {
+        const parentCard = e.target.parentElement
+        parentCard.appendTo(cartContainer)
+
+    }))
+}
+
+const skipCards3 = () => {
+    const skipBtn = document.querySelectorAll('.skip-btn')
+    skipBtn.forEach(btn => btn.addEventListener('click', e => {
+        const parentCard = e.target.parentElement
+        parentCard.classList.add('d-none')
+    }))
+}
